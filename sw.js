d0c2898c78fdeb0ca0d4afd869b751bde3978f55
hktcr <132231577+hktcr@users.noreply.github.com>
2026-08-11 10:16:27 +0200

    Aktivera nya timersignaler

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_PREFIX = 'vavr-shell-';
-const CACHE_NAME = CACHE_PREFIX + '2026-08-11-01';
+const CACHE_NAME = CACHE_PREFIX + '2026-08-11-02';
 const SHELL_PATHS = [
   './',
   './index.html',
